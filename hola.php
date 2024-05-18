070482c8914686b65a7a3bb5291681f42889f648
pramgar672 <pablomanuelrg@gmail.com>
2024-05-18 22:53:43 +0200

    Se añade un parámetro por defecto pmrg

--- hola.php
+++ hola.php
@@ -1,2 +1,3 @@
 <?php
-echo "Hola Mundo, soy pablo y esta es la tarea presencial 5 modificado\n";
+$nombre = isset($argv[1]) ? $argv[1] : "Mundo";
+@print "Hola Mundo, soy pablo y esta es la tarea presencial 5, {$nombre}\n";
