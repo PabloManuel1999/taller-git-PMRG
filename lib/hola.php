5bd84a839f143265623026b0c51bf819f2ad2e39
pramgar672 <pablomanuelrg@gmail.com>
2024-05-19 14:13:20 +0200

    hola usa la clase HolaMundo PMRG

--- lib/hola.php
+++ lib/hola.php
@@ -2,4 +2,4 @@
 //Autor: Pablo Manuel Ramos Garrido <pablo@gmail.com>
 
 $nombre = isset($argv[1]) ? $argv[1] : "Mundo";
-@print "Hola Mundo, soy pablo y esta es la tarea presencial 5, {$nombre}\n";
+@print "Hola Modificado, soy pablo y esta es la tarea presencial 5, {$nombre}\n";
